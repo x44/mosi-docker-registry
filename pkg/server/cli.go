bcc3aeb7ba3ce9ec0be2e60fb49a7b1da6f7023d
x44 <damichl73@gmx.at>
2022-12-18 09:43:55 +0100

    Using json

--- pkg/server/cli.go
+++ pkg/server/cli.go
@@ -7,7 +7,7 @@ import (
 )
 
 // /v2/cli/...
-func handleGetCli(w http.ResponseWriter, r *http.Request) {
+func cliHandleGet(w http.ResponseWriter, r *http.Request) {
 	if !checkAdminAuth(w, r) {
 		return
 	}
@@ -24,15 +24,15 @@ func handleGetCli(w http.ResponseWriter, r *http.Request) {
 
 	switch cmd {
 	case "ls":
-		handleGetCliList(w, paths)
+		cliHandleGetList(w, paths)
 	default:
 		sendError(w, 400, "BAD REQUEST", "Unknown command '"+cmd+"'")
 	}
 }
 
-func handleGetCliList(w http.ResponseWriter, args []string) {
-	img := "*"
-	tag := "*"
+func cliHandleGetList(w http.ResponseWriter, args []string) {
+	img := ""
+	tag := ""
 	if len(args) > 0 {
 		img, tag = getImageAndTag(args[0])
 	}
@@ -48,9 +48,13 @@ func handleGetCliList(w http.ResponseWriter, args []string) {
 }
 
 func getImageAndTag(s string) (string, string) {
-	if len(s) == 0 || s == ":" {
+	if len(s) == 0 {
+		return "*", ""
+	}
+	if s == ":" {
 		return "*", "*"
 	}
+
 	var img string
 	var tag string
 	a := strings.Split(s, ":")
@@ -59,17 +63,19 @@ func getImageAndTag(s string) (string, string) {
 			// s = ":tag", a = [tag]
 			img, tag = "*", a[0]
 		} else {
-			// s = "img:" or "img", a = [img]
-			img, tag = a[0], "*"
+			// s = "img", a = [img]
+			img, tag = a[0], ""
 		}
 	} else {
 		img, tag = a[0], a[1]
+
+		if len(a[1]) == 0 {
+			// s = "img:", a = [img,""]
+			tag = "*"
+		}
 	}
-	if len(img) == 0 {
+	if tag != "" && img == "" {
 		img = "*"
-	}
-	if len(tag) == 0 {
-		tag = "*"
 	}
 	return img, tag
 }
